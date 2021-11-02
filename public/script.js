f5bc173e3f6c37c22a84f105f8a92294ac2ca861
amunium <age7@hotmail.no>
2021-11-02 14:55:14 +0100

    start add firebase

--- public/script.js
+++ public/script.js
@@ -1,3 +1,22 @@
+import { initializeApp } from 'firebase/app';
+// TODO: import database or firestore...
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    //TODO: fyll ut teksten
+    apiKey: "API_KEY",
+    authDomain: "PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://PROJECT_ID.firebaseio.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "G-MEASUREMENT_ID",
+};
+
+//initialising firebase within the web-application
+const app = initializeApp(firebaseConfig);
+
 
 // initiates page with the latest update
 function pageInit() {
