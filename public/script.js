61a505d2ade37fd991706984e177a5e44349462f
amunium <age7@hotmail.no>
2021-11-02 14:39:35 +0100

    add script.js with bare functions

---
+++ public/script.js
@@ -0,0 +1,26 @@
+
+// initiates page with the latest update
+function pageInit() {
+    // create head menu with topics
+    // create right menu with index
+    // create left meny with startup menu
+}
+
+
+// changes topic
+function changeTopic(topicName) {
+    // change title to topic name
+
+    // change text to topic text
+
+    // change left menu
+    
+}
+
+// changes text
+function changeText(textName) {    
+    // change text
+
+    // change sources
+
+}
