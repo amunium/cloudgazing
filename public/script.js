43d9d5045adaa7efe31eb6eddcab7fa0d4859b8b
amunium <age7@hotmail.no>
2021-11-03 16:04:39 +0100

    add firebase, fix topbar, add bare movement functions

--- public/script.js
+++ public/script.js
@@ -1,43 +1,50 @@
-import { initializeApp } from 'firebase/app';
-// TODO: import database or firestore...
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    //TODO: fyll ut teksten
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://PROJECT_ID.firebaseio.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
-    measurementId: "G-MEASUREMENT_ID",
+  apiKey: "AIzaSyCYJ9QtQi2oRG82GsgJ7_KDfEGZpWW6w7g",
+  authDomain: "cloudgazing.firebaseapp.com",
+  databaseURL: "https://cloudgazing-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cloudgazing",
+  storageBucket: "cloudgazing.appspot.com",
+  messagingSenderId: "30452503596",
+  appId: "1:30452503596:web:d6cc65fde6fb1de558ed89"
 };
 
-//initialising firebase within the web-application
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 // initiates page with the latest update
 function pageInit() {
     // create head menu with topics
+
     // create right menu with index
+
     // create left meny with startup menu
+    alert();
 }
 
 
 // changes topic
-function changeTopic(topicName) {
-    // change title to topic name
+function changeTopic(topicId) {
+    //get snapshot of topicDatabase
+
+    // change title to topic title
 
     // change text to topic text
 
     // change left menu
     
+    // change active topbar element
+    document.getElementsByClassName("active")[0].className = " ";
+    document.getElementById("p" + topicId).className = "active";
 }
 
 // changes text
-function changeText(textName) {    
+function changeText(topicId, contentId) {    
     // change text
 
     // change sources
